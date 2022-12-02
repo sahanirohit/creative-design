6196ec9f429468f59e6581636ce8d6100fd77735
Rohit Sahani <72742354+sahanirohit@users.noreply.github.com>
2022-12-02 14:32:21 +0530

    robot.txt added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,6 @@ import { Fragment } from "react";
 import About from "../components/About";
 import Company from "../components/Company";
 import Connect from "../components/Connect";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
 
